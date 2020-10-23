2c2f5288ce03b3ffeab8bb3aceb3592213a32995
Azure ML <azureml@microsoft.com>
2020-10-23 15:46:22 +0000

    removed out of date sdk

--- sdk2-src/src/cli/src/machinelearningservices/azext_ml/manual/_params/_job_params.py
+++
@@ -1,9 +0,0 @@
-# ---------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# ---------------------------------------------------------
-
-
-def load_job_params(self):
-    with self.argument_context('ml code-job create') as c:
-        c.argument('file', help="yaml file to generate code job from")
-        c.argument('save_as', help="file to save code job to")
