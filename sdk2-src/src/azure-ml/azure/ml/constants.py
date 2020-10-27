eace2e11d08b2b2bfda653f9cf0d4a0e18ad87d0
Azure ML <azureml@microsoft.com>
2020-10-27 23:16:29 +0000

    removed out of date sdk

--- sdk2-src/src/azure-ml/azure/ml/constants.py
+++
@@ -1,9 +0,0 @@
-# ---------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# ---------------------------------------------------------
-
-MFE_BASE_URL = "https://centraluseuap.management.azure.com"
-API_VERSION_2020_12_01_PREVIEW = "2020-12-01-preview"
-API_VERSION_2020_09_01_PREVIEW = "2020-09-01-preview"
-ONLINE_ENDPOINT_TYPE = "online"
-BATCH_ENDPOINT_TYPE = "batch"
