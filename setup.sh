5fcd624f14b7ec01016408290a2fba365a8f8ba9
Jordan Edwards <jordane@microsoft.com>
2020-11-05 12:46:08 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@ conda activate cli_v2
 pip install azure-cli
 pip install --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2 azure-ml==0.0.1
 wget https://paulshaml18829289173.blob.core.windows.net/wheels/ml-0.1.0-py3-none-any.whl
+az extension remove -n azure-cli-ml
 az extension add --source ml-0.1.0-py3-none-any.whl -y
