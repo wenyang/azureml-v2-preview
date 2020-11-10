b8fa3d7c2aa9f45ee8f7a47e7bdbf0728f38b2a1
Azure ML <azureml@microsoft.com>
2020-11-10 05:43:08 +0000

    removed out of date sdk

--- sdk2-src/samples/python/hello_world.py
+++
@@ -1,1 +0,0 @@
-print("Can write some train/test/score scripts, but first, Hello World.")
--- sdk2-src/tests/azure-ml/utilities/utils.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def write_script(script_path: str, content: str) -> str:
-    """
-    Util for generating a python script, currently writes the file to disk.
-    """
-    with open(script_path, "w") as stream:
-        stream.write(content)
-    return script_path
