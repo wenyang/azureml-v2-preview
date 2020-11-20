bc421eee9d0376186f827e607e753697f085b81e
Jordan Edwards <jordane@microsoft.com>
2020-11-20 14:03:06 -0800

    Update sidebars.js

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,36 +1,5 @@
 module.exports = {
   userguide:{
-    'User Guide': [
-      {
-        type: 'doc',
-        id: 'userguide/README',
-        label: 'Start Here!'
-      },
-      {
-        type: 'doc',
-        id: 'userguide/job',
-        label: 'Model Training'
-      },
-      {
-        type: 'doc',
-        id: 'userguide/endpoint',
-        label: 'Model Deployment'
-      },
-      {
-        type: 'doc',
-        id: 'userguide/compute',
-        label: 'Compute Management'
-      },
-      {
-        type: 'doc',
-        id: 'userguide/data',
-        label: 'Data Management'
-      },
-      {
-        type: 'doc',
-        id: 'userguide/model',
-        label: 'Model Management'
-      }
-    ],
+      ['userguide/job','userguide/endpoint']
   }
 };
