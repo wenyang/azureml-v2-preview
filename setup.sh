1f4df403e7a007c593ec3ce0ace572444cf6abc4
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2020-12-09 12:56:10 -0800

    Delete setup.sh (#66)

--- setup.sh
+++
@@ -1,6 +0,0 @@
-pip install --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2 azure-ml==0.0.1
-wget https://mldevplatv2.blob.core.windows.net/cli/cli.zip
-mkdir ~/azmlcli; tar xvf cli.zip -C ~/azmlcli
-export AZURE_EXTENSION_DIR=~/azmlcli
-
-az ml -h
