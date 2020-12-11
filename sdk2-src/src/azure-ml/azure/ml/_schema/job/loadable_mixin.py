e5e7ca05a29a176979fcc4b904f72ac87f34810f
Azure ML <azureml@microsoft.com>
2020-12-11 21:26:29 +0000

    removed out of date sdk

--- sdk2-src/src/azure-ml/azure/ml/_schema/job/loadable_mixin.py
+++
@@ -1,9 +0,0 @@
-# ---------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# ---------------------------------------------------------
-from typing import Any
-
-
-class LoadableMixin:
-    def load(self, obj: Any) -> None:
-        pass
