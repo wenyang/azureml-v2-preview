9a3060ebd0515c8eb484222e05e155739ca6e3dc
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2020-12-12 11:33:09 -0800

    switch to furo theme

--- docs/conf.py
+++ docs/conf.py
@@ -51,9 +51,17 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme' # standard rtd theme
+html_theme = 'furo' # pypi theme
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_theme_options = {
+    "light_css_variables": {
+        "color-brand-primary": "#CC66FF",
+        "color-brand-content": "purple",
+    },
+}
