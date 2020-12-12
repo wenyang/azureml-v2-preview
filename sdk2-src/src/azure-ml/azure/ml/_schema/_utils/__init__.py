6111b851d47eb20b35830fe9327b68f263b1284f
Azure ML <azureml@microsoft.com>
2020-12-12 04:00:04 +0000

    removed out of date sdk

--- sdk2-src/src/azure-ml/azure/ml/_schema/_utils/__init__.py
+++
@@ -1,9 +0,0 @@
-# ---------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# ---------------------------------------------------------
-
-__path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
-
-from .utils import load_job_from_yaml
-
-__all__ = ["load_job_from_yaml"]
