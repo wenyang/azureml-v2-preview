7424ce1698ca6ae544292ae5833a3275f43777c0
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2020-12-13 14:30:43 -0800

    updates

--- docs/conf.py
+++ docs/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'azure.ml'
-copyright = '2020, Microsoft'
-author = 'Microsoft'
+project = "azure.ml"
+copyright = "2020, Microsoft"
+author = "Microsoft"
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -35,17 +35,17 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "numpydoc",
-    "sphinx_copybutton"
-
+    "sphinx_copybutton",
+    "sphinx_togglebutton",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -53,13 +53,13 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'sphinx_rtd_theme' # standard rtd theme
-html_theme = 'furo' # pypi theme
+# html_theme = 'sphinx_rtd_theme' # standard rtd theme
+html_theme = "furo"  # pypi theme
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 html_theme_options = {
     "light_css_variables": {
--- docs/overview/blitz.rst
+++ docs/overview/blitz.rst
@@ -1,16 +1,21 @@
-Hello World
-===========
+Tutorial
+========
 
 Follow this 10 minute tutorial to get started with `azure.ml`.
 
 Hello World!
 ------------
 
-Let's start by running our first job tracked in the cloud:
+.. toggle::
 
-.. code-block:: console
+    .. warning::
+        Local execution is not in scope for private preview.
 
-    az ml job create --name
+    Let's start by running our first job tracked in the cloud:
+
+    .. code-block:: console
+
+        az ml job create --name
 
 Hello Azure!
 ------------
@@ -30,15 +35,15 @@ Let's train a simple lightgbm model on the Iris dataset.
 
     az ml job create --name 
 
-Tune Hyperparameters
---------------------
+Tune parameters
+---------------
 
 .. code-block:: console
 
     az ml job create --name 
 
-Deploy model to endpoint
-------------------------
+Deploy to endpoint
+------------------
 
 .. code-block:: console 
 
