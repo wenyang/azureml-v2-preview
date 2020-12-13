593a2b683e98f2697e0028cd5a8cb26c3fca9f6b
Jordan Edwards <jordane@microsoft.com>
2020-12-12 16:24:25 -0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,8 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "numpydoc",
+    "sphinx_copybutton"
+
 ]
 
 # Add any paths that contain templates here, relative to this directory.
