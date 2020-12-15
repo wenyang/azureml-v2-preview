ebe691aede597d0368a989aa4385c203be2cf74d
Jordan Edwards <jordane@microsoft.com>
2020-12-14 16:35:50 -0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -54,16 +54,17 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 #
 # html_theme = 'sphinx_rtd_theme' # standard rtd theme
-html_theme = "furo"  # pypi theme
+# html_theme = "furo"  # pypi theme
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-html_theme_options = {
-    "light_css_variables": {
-        "color-brand-primary": "#CC66FF",
-        "color-brand-content": "purple",
-    },
-}
+#html_theme_options = {
+#    "light_css_variables": {
+#        "color-brand-primary": "#CC66FF",
+#        "color-brand-content": "purple",
+#    },
+#}
